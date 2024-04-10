63968763a13c65f164907f8630fc82813130e1a1
MichelaVivacqua <vivacquamichela@gmail.com>
2024-04-10 14:46:37 +0200

    aggiunta di relazione tra autore e blogPost

--- src/main/java/michelavivacqua/w6d31044/entities/Autore.java
+++ src/main/java/michelavivacqua/w6d31044/entities/Autore.java
@@ -1,21 +1,28 @@
 package michelavivacqua.w6d31044.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
+@Entity
 public class Autore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private String cognome;
     private String email;
     private LocalDate dataDiNascita;
     private String avatar;
+    @OneToMany(mappedBy = "autore")
+    private List<BlogPost> blogPosts;
 }
 //public void setAvatar() {
 //    this.avatar = "https://ui-avatars.com/api/?name=" + this.name + "+" + this.surname;
--- src/main/java/michelavivacqua/w6d31044/entities/BlogPost.java
+++ src/main/java/michelavivacqua/w6d31044/entities/BlogPost.java
@@ -1,5 +1,6 @@
 package michelavivacqua.w6d31044.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,13 +8,17 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor
-
+@Entity
 public class BlogPost {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String categoria;
     private String titolo;
     private String cover;
     private String contenuto;
     private int tempoDiLettura;
+    @ManyToOne
+    @JoinColumn(name = "autore_id")
+    private Autore autore;
 }
