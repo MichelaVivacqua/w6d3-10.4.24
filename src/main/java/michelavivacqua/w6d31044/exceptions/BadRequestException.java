d59d3c05f8924368910f07b9ec25a244937687e5
MichelaVivacqua <vivacquamichela@gmail.com>
2024-04-10 14:53:50 +0200

    gestione errori

---
+++ src/main/java/michelavivacqua/w6d31044/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package michelavivacqua.w6d31044.exceptions;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message){
+        super(message);
+    }
+}
